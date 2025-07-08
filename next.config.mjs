1ad7c79acae3497549bc63c8708222ab9fef67ec
Ranga McMil <ranga@Rangas-MacBook-Air.local>
2025-07-08 13:13:53 +0200

    staleTimes item

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
+  }
 }
 
 export default nextConfig
